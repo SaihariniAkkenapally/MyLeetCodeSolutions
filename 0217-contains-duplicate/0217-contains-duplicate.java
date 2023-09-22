98243866fc198c84897f455e249119fa820abdad
SaihariniAkkenapally <113228374+SaihariniAkkenapally@users.noreply.github.com>
2023-09-22 11:59:15 +0530

    Time: 21 ms (11.28%), Space: 58.2 MB (6.21%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution
+{
+    public boolean containsDuplicate(int[] nums)
+    {
+        Arrays.sort(nums);
+        for(int index = 0 ; index < nums.length - 1 ; index++)
+        {
+            if(nums[index] == nums[index + 1])
+                return true;
+        }
+        return false;
+    }
+}
