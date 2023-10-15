6d14236e5fe31b7c873b45fc5441364d54225872
SaihariniAkkenapally <113228374+SaihariniAkkenapally@users.noreply.github.com>
2023-10-15 15:40:02 +0530

    Time: 0 ms (100%), Space: 40.4 MB (68.08%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,41 +1,26 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode tempNode = head,prevNode = null;
-        int length = 0;
-        while(tempNode != null)
-        {
-            length++;
-            tempNode = tempNode.next;
-        }
-        if(n == 1 && length == 1)
+        if(head == null || head.next == null) 
             return null;
-        if(length == n)
-            return head.next;
-        int i = 0;
-        n = length - n;
-        tempNode = head;
-        while(tempNode != null && i < n)
+        
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+        ListNode firstNode = dummyNode;
+        ListNode secondNode = dummyNode;
+        
+        while(n > 0)
         {
-            prevNode = tempNode;
-            tempNode = tempNode.next;
-            i++;
+            secondNode = secondNode.next;
+            n--;
         }
-        if(tempNode.next == null)
-            prevNode.next = null;
-        else
+        
+        while(secondNode.next != null)
         {
-            prevNode.next = tempNode.next;
+            secondNode = secondNode.next;
+            firstNode = firstNode.next;           
         }
-        return head;
+        firstNode.next = firstNode.next.next;
+        
+        return dummyNode.next;
     }
 }
