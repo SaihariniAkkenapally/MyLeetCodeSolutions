0e9b4fefc9034ac76342e8e1e71145d059f8c83b
SaihariniAkkenapally <113228374+SaihariniAkkenapally@users.noreply.github.com>
2023-10-16 17:23:13 +0530

    Time: 1 ms (100%), Space: 42.9 MB (92.62%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,29 +9,53 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
-        if(head1 == null) return head2;
-        if(head2 == null) return head1;
-        if(head1.val <= head2.val)
-        {
-            head1.next = mergeTwoLists(head1.next, head2);
-            return head1;
-        }
-        else{
-            head2.next = mergeTwoLists(head1, head2.next);
-            return head2;
-        }
-    }
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists.length == 0) 
             return null;
         if(lists.length == 1)
             return lists[0];
-        ListNode temp = mergeTwoLists(lists[0], lists[1]);
-        for(int index = 2; index < lists.length; index++)
+        return mergeSortOfLists(lists, 0, lists.length - 1);
+    }
+
+    ListNode mergeSortOfLists(ListNode[] lists, int left, int right)
+    {
+        if(right < left)
+            return null;
+        if(left == right)
+            return lists[left];
+        int mid = left + (right - left)/2;
+
+        ListNode list1 = mergeSortOfLists(lists, left, mid);
+        ListNode list2 = mergeSortOfLists(lists, mid + 1, right);
+
+        return mergeTwoLists(list1, list2);
+    }
+
+    ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode(-1);
+        ListNode currentNode = head; 
+        
+        while(list1 != null && list2 != null)
         {
-            temp = mergeTwoLists(temp, lists[index]);
+            if(list1.val <= list2.val)
+            {
+                currentNode.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                currentNode.next = list2;
+                list2 = list2.next;
+            }
+            currentNode = currentNode.next;
         }
-        return temp;
+        
+        if(list1 != null && list2 == null)
+            currentNode.next = list1;
+        
+        if(list2 != null && list1 == null)
+            currentNode.next = list2;
+        
+        return head.next;
     }
 }
