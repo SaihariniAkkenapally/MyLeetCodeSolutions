6653853e2f45550bc038d2d15da57c31eb680c61
SaihariniAkkenapally <113228374+SaihariniAkkenapally@users.noreply.github.com>
2024-07-09 20:02:03 +0530

    Time: 3 ms (42.47%), Space: 42.5 MB (8.61%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.java
+++ 1217-relative-sort-array/1217-relative-sort-array.java
@@ -2,25 +2,37 @@ class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
         int[] result = new int[arr1.length];
         int index = 0;
+    
+        Map<Integer, Integer> map = new HashMap();
+
+        for(int ele : arr1)
+            map.put(ele, map.getOrDefault(ele,0)+1);
+
         for(int i = 0; i < arr2.length; i++)
         {
-            for(int j = 0; j < arr1.length; j++)
+            while(map.containsKey(arr2[i]))
             {
-                if(arr2[i] == arr1[j])
-                {
-                    result[index++] = arr2[i];
-                    arr1[j] = -1;
-                }
+                result[index++] = arr2[i];
+                map.put(arr2[i], map.get(arr2[i])-1);
+                if(map.get(arr2[i]) == 0)
+                    map.remove(arr2[i]);
             }
         }
-        Arrays.sort(arr1);
+        int[] extraElements = new int[arr1.length - index];
+        int i = 0;
 
-        for(int i = 0; i < arr1.length; i++)
+        for(int key : map.keySet())
         {
-            if(arr1[i] > -1)
-                result[index++] =  arr1[i];
+            while(map.containsKey(key) && map.get(key) != 0)
+            {
+                extraElements[i++] = key;
+                map.put(key, map.get(key)-1);
+            }
         }
-        
+        Arrays.sort(extraElements);
+        for(int ele : extraElements)
+            result[index++] = ele;
+    
         return result;
     }
 }
