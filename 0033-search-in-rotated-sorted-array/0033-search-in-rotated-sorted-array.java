b36e1b0358cc8d4928d7a2a853777d04a694a2c4
SaihariniAkkenapally <113228374+SaihariniAkkenapally@users.noreply.github.com>
2024-07-18 11:38:53 +0530

    Time: 0 ms (100%), Space: 41.9 MB (40.95%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -9,7 +9,7 @@ class Solution {
 
             if(nums[mid] == target)
                 return mid;
-
+            //left sorted array
             if(nums[left] <= nums[mid])
             {
                 if(target >= nums[left] && target < nums[mid])
@@ -17,12 +17,12 @@ class Solution {
                 else
                     left = mid + 1;
             }
-            else
+            else if(nums[mid] <= nums[right])//right sorted array
             {
-                if(target < nums[mid] || target > nums[right])
-                    right = mid - 1;
-                else 
+                if(target > nums[mid] && target <= nums[right])
                     left = mid + 1;
+                else 
+                    right = mid - 1;
             }
         }
         return -1;
